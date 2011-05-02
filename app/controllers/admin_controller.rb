395f046e167b3dd68e81a8ac3aa0c1b3065e53b9
Joshua Toliver <jtoliver@mailbox.sc.edu>
2011-05-02 10:39:16 -0400

    seventh commit

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,27 +1,28 @@
 class AdminController < ApplicationController
+  require 'csv'
   before_filter :check_if_admin
   
   def index
-    @items = Item.includes(:program).all
+    @items = Item.joins(:program).all
   end
   
-  #def export_to_csv
-    #@items = Item.includes(:program).all
+  def export_to_csv
+    @items = Item.joins(:program).all
     
-   # csv_string = FasterCSV.generate do |csv|
+    csv_string = CSV.generate do |csv|
       #header
-     # csv << ["Department", "Fund", "Program", "Priority", "Program Status", "Purpose", "Desc", "link", "code", "details", "amount", "updated"]
+      csv << ["Department", "Fund", "Program", "Priority", "Program Status", "Purpose", "Desc", "link", "code", "details", "amount", "updated"]
     
       #rows
-      #@items.each do |item|
-      #csv << [item.program.dept, item.program.fund, item.program.program, item.program.priority, item.program.prog_status, item.program.purpose, item.program.desc, item.program.link, item.obj_code, item.details, item.amount, item.updated_at]
-      #end   
-    #end
+      @items.each do |item|
+      csv << [item.program.dept, item.program.fund, item.program.program, item.program.priority, item.program.prog_status, item.program.purpose, item.program.desc, item.program.link, item.obj_code, item.details, item.amount, item.updated_at]
+      end
+    end
   
     #browser
-   # send_data csv_string,
-     # :type => 'text/csv; charset=iso-8859-1; header=present',
-     # :disposition => "attachment; filename=users.csv"  
-  #end
+    send_data csv_string,
+      :type => 'text/csv; charset=iso-8859-1; header=present',
+      :disposition => "attachment; filename=users.csv"
+  end
 
 end
